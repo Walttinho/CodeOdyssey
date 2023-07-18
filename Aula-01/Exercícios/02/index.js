25dadc980b3f09d76def044ced226b7d5b5a4f93
Walter.Netto <walter.netto@live.com>
2023-07-18 17:07:49 -0300

    add: exercise list solved

---
+++ Aula-01/Exercícios/02/index.js
@@ -0,0 +1,7 @@
+// celsius é igual Fahrenheit menos 32 vezes 5 sobre 9
+
+let fahrenheit = 80;
+let celsius;
+
+celsius = (fahrenheit - 32) * (5 / 9);
+console.log(`A temperatura é de ${celsius} celsius`);
---
+++ Aula-01/Exercícios/03/index.js
@@ -0,0 +1,8 @@
+// dinheiro menos valorProduto divido pelo valorProduto vezes 100
+let dinheiro = 300
+let valorProduto = 500
+let descNecessario
+
+descNecessario = ((valorProduto - dinheiro) / valorProduto) * 100
+
+console.log(`O valor de desconto necessário é de ${descNecessario}%`)
