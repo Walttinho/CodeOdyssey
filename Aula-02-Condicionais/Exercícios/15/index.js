b4fbc6aa9d312e9fc225e8543a3d7ffe5d223258
Walter.Netto <walter.netto@live.com>
2023-07-20 17:41:18 -0300

    feat: display name

---
+++ Aula-02-Condicionais/Exercícios/15/index.js
@@ -0,0 +1,12 @@
+const primeiroNome = "Mario";
+const sobrenome = "";
+const apelido = "";
+
+//seu código aqui
+if (apelido){
+console.log(apelido)
+}else if(primeiroNome && sobrenome){
+  console.log(primeiroNome+" "+sobrenome)
+}else{
+  console.log( primeiroNome)
+}
