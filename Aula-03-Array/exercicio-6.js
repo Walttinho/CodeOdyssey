7ea7d3adb8ebe93d7415499a36580855668700bb
Walter.Netto <walter.netto@live.com>
2023-07-21 00:15:42 -0300

    solved

--- Aula-03-Array/exercicio-6.js
+++ Aula-03-Array/exercicio-6.js
@@ -1,6 +1,8 @@
-let numero = 2;
-
-while (numero <= 50) {
-    console.log(numero);
-    numero += 2;
+// imprime todos numeros pares de 1 a 50
+let numero = 1
+while(numero <= 50){
+  if(numero % 2 == 0){
+    console.log(numero)
+  }
+  ++numero
 }
