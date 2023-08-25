8b6b319159b16bc9524deab51e6898d83723ae2f
Walttinho <walter.netto@live.com>
2023-08-25 01:17:27 -0300

    aula API-Rest

---
+++ Modulo-02/Aula-03-API-Rest/API-rest/src/routes/instrutor.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const instrutores = require("../controller/instrutor.controller");
+
+const rotas = express();
+
+rotas.get("/instrutores", instrutores.findAll);
+rotas.get("/instrutores/:id", instrutores.findById);
+rotas.post("/instrutores", instrutores.create);
+rotas.put("/instrutores/:id", instrutores.update);
+rotas.patch("/instrutores/:id", instrutores.updateStatus);
+rotas.delete("/instrutores/:id", instrutores.remove);
+
+module.exports = rotas;
