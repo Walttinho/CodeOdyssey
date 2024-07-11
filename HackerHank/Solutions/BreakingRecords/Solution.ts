6ad5b166c9263deab2b80f4bb5463e6068fd427c
walttinho <walter.netto@live.com>
2024-07-11 16:52:25 -0300

    olved

---
+++ HackerHank/Solutions/BreakingRecords/Solution.ts
@@ -0,0 +1,67 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+function breakingRecords(scores: number[]): number[] {
+    // Write your code here
+    let max = scores[0]; 
+    let min = scores[0]; 
+    let countMax = 0; 
+    let countMin = 0; 
+
+    for (const score of scores) {
+        if (score > max) {
+            max = score;
+            countMax++;
+        }
+        if (score < min) {
+            min = score;
+            countMin++;
+        }
+    }
+
+    return [countMax, countMin];
+
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const n: number = parseInt(readLine().trim(), 10);
+
+    const scores: number[] = readLine().replace(/\s+$/g, '').split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const result: number[] = breakingRecords(scores);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
